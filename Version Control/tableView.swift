1b1f6ea61dec548de74f10b861e820f9644d9ca5
nivethetha-r <105229513+nivethetha-r@users.noreply.github.com>
2023-05-16 11:11:03 +0530

    Adding a tableView

---
+++ Version Control/tableView.swift
@@ -0,0 +1,20 @@
+//
+//  tableView.swift
+//  Version Control
+//
+//  Created by nivethe-pt6092 on 16/05/23.
+//
+
+import UIKit
+
+class tableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
