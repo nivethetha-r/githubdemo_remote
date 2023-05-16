7cb95a6d8773be72c07c186137954746ecfcc2d0
nivethetha-r <105229513+nivethetha-r@users.noreply.github.com>
2023-05-16 11:43:05 +0530

    Pushing to the remote repository

--- Version Control/ViewController.swift
+++ Version Control/ViewController.swift
@@ -12,13 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .red
         let albums = ["Red", "1989", "Reputation"]
 
         for album in albums {
             print("\(album) is on Apple Music")
         }
     }
-
-
 }
 
